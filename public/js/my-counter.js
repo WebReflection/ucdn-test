9e15734d31e33d5526d6aa1943903d75be61ca1b
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-08 20:48:45 +0200

    updated published folder

--- public/js/my-counter.js
+++ public/js/my-counter.js
@@ -1,5 +1,1 @@
-import{define}from"../node_modules/uce/esm/index.js";define("my-counter",{init(){this.count=0,this.dec=(()=>{this.count--,this.render()}),this.inc=(()=>{this.count++,this.render()}),this.render()},render(){this.html`
-      <button onclick="${this.dec}">-</button>
-      <span>${this.count}</span>
-      <button onclick="${this.inc}">+</button>
-    `}});
+import{define}from"../node_modules/uce/esm/index.js";define("my-counter",{init(){this.count=0,this.dec=()=>{this.count--,this.render()},this.inc=()=>{this.count++,this.render()},this.render()},render(){this.html`<button onclick=${this.dec}>-</button> <span>${this.count}</span> <button onclick=${this.inc}>+</button>`}});
