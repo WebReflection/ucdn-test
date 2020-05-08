dc79245f33d2446301e393c4014af8bf839bd71c
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-08 17:31:28 +0200

    pointing at package.json via relative path

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 import{render,html}from"../node_modules/uhtml/esm/index.js";import"./my-counter.js";addEventListener("DOMContentLoaded",()=>{render(document.body,html`
       <my-counter />
       <footer></footer>
-    `),fetch("/package.json").then(e=>e.json()).then(({description:e,version:o})=>{render(document.querySelector("footer"),html`
+    `),fetch("../package.json").then(e=>e.json()).then(({description:e,version:o})=>{render(document.querySelector("footer"),html`
           ${e} v${o}
         `)})},{once:!0});
