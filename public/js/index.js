7850d880d82b8cbb9f5dc78d66ece404c7cb297f
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-14 10:53:21 +0200

    added --verbose

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,1 @@
-import{render as e,html as o}from"../node_modules/uhtml/esm/index.js";import"./my-counter.js";addEventListener("DOMContentLoaded",()=>{e(document.body,o`
-      <my-counter />
-      <footer></footer>
-    `),fetch("package.json").then(e=>e.json()).then(({description:t,version:n})=>{e(document.querySelector("footer"),o`
-          ${t} v${n}
-        `)})},{once:!0});
+import{render as e,html as o}from"../node_modules/uhtml/esm/index.js";import"./my-counter.js";addEventListener("DOMContentLoaded",()=>{e(document.body,o`<my-counter/><footer></footer>`),fetch("package.json").then(e=>e.json()).then(({description:t,version:n})=>{e(document.querySelector("footer"),o`${t} v${n}`)})},{once:!0});
